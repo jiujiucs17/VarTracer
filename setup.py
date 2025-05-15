45a6271b2e6c1e8ad3095026f467db9ead1f9b12
jiujiucs17 <jiujiuchangshou@sina.com>
2025-05-15 13:37:18 -0400

    repackaged everything and reverted version code to 0.1.0

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='VarTracer',          # 包名，需唯一，PyPI 上不能重复
-    version='0.1.1',                   # 语义化版本号
-    author='Mengqi Zhang',
-    author_email='jiujiuchangshou@gmail.com',
-    description='A hybrid analysis tool for tracing variable dependencies across python codes',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',  # 告诉 PyPI README 用的是 markdown
-    url='https://github.com/jiujiucs17/VarTracer',  # 项目主页，如 GitHub
-    packages=find_packages(exclude=["tests*"]),     # 自动发现子包
-    classifiers=[                                    # PyPI 分类标准
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',   # 或 BSD, GPL, Apache 等
-        'Operating System :: OS Independent',
+    name="VarTracer",
+    version="0.1.0",
+    description="A Python-based tool for dynamic code execution tracing and dependency analysis.",
+    long_description=open("readme.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Mengqi Zhang",
+    author_email="jiujiuchangshou@gmail.com",
+    url="https://github.com/jiujiucs17/VarTracer",
+    packages=find_packages(include=["VarTracer", "VarTracer.*"]),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    include_package_data=True,
 )
